1292c313940ca863e6f42858010377e2a2dfd1ab
Volodymyr <Volodymyr@YOGASLIM7>
2023-01-23 22:27:02 +0200

    Add project files.

---
+++ ServiceSolution.Core/Bus.cs
@@ -0,0 +1,13 @@
+﻿using ServiceSolution.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceSolution.Domain
+{
+    public class Bus : Vehicle
+    {
+    }
+}
