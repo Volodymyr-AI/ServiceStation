28dadbee0e9e953ef6d4170a1230607d0be3cb80
Volodymyr Moiseichenko <alternativeqoob@gmail.com>
2023-02-14 13:42:18 +0200

    After this commit I will rewrite fully this app because of installation problems with nuget and visual studio

--- ServiceStation.Persistence/EntityTypeConfiguration/VehicleConfiguration.cs
+++ ServiceStation.Persistence/EntityTypeConfiguration/VehicleConfiguration.cs
@@ -1,12 +1,6 @@
-﻿using FluentValidation;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using ServiceStation.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ServiceStation.Persistence.EntityTypeConfiguration
 {
---
+++ ServiceStationAPI/Program.cs
@@ -0,0 +1,15 @@
+namespace ServiceStationAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
