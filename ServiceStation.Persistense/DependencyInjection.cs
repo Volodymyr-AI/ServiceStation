2318c44b46703f9e8f1d532f287bd01fe72468c4
Volodymyr Moiseichenko <alternativeqoob@gmail.com>
2023-03-02 18:56:02 +0200

    Added DependencyInjection classes for further integration into WebAPI. Also deleted repository with empty dtos frfom persistence, do not know how they even appeared there

---
+++ ServiceStation.Persistense/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceStation.Application.Interfaces;
+
+namespace ServiceStation.Persistense
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistense(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            services.AddScoped<IAppDbContext>(provider =>
+                (IAppDbContext)provider.GetService<AppDbContext>());
+            return services;
+        }
+    }
+}
